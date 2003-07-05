1985f2519e579b38d1b4ac62c1a50fb804d465d1
Joshua ben Jore <jjore@cpan.org>
2003-07-05 19:19:56 +0000

    Import of JJORE/B-Deobfuscate-0.06 from CPAN.
    gitpan-cpan-distribution: B-Deobfuscate
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         JJORE/B-Deobfuscate-0.06.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

--- lib/B/Deobfuscate.pm
+++ lib/B/Deobfuscate.pm
@@ -1,14 +1,13 @@
 package B::Deobfuscate;
 use strict;
 use warnings;
-use vars '$VERSION';
 use base 'B::Deparse';
 use B ();
 use B::Keywords ();
 
 # Some functions may require() YAML
 
-$VERSION = '0.03';
+our $VERSION = '0.06';
 
 sub load_keywords {
     my $self = shift;
@@ -25,20 +24,26 @@ sub load_unknown_dict {
     my $self = shift;
     my $p = $self->{+__PACKAGE__};
     my $dict_file = $p->{'unknown_dict_file'};
-    length $dict_file or return;
     my $dict_data;
 
     # slurp the entire dictionary at once
-    open DICT, '<', $dict_file
-        or die "Cannot open dictionary $dict_file: $!";
-    read DICT, $dict_data, -s DICT;
-    close DICT or die "Cannot close $dict_file: $!";
+    if ($dict_file) {
+        local *DICT;
+        open DICT, '<', $dict_file
+            or die "Cannot open dictionary $dict_file: $!";
+        read DICT, $dict_data, -s DICT;
+        close DICT or die "Cannot close $dict_file: $!";
+    } else {
+        # Use the built-in symbol list
+        local $/;
+        $dict_data = readline *DATA;
+    }
 
     my $k = $self->load_keywords;
 
     $p->{'unknown_dict_data'} =
         [ sort { length $a <=> length $b or $a cmp $b }
-          grep { ! /\W/ and ! exists $k->{$_} }
+          grep { length > 3 and ! /\W/ and ! exists $k->{$_} }
           split /\n/, $dict_data ];
 }
 
@@ -94,13 +99,15 @@ sub load_user_config {
 
 sub gv_should_be_renamed {
     my $self = shift;
+    my $sigil = shift;
     my $name = shift;
     my $p = $self->{+__PACKAGE__};
     my $k = $p->{'keywords'};
 
     # Ignore keywords
-    return if exists $k->{$name} or
-              $name =~ m{\A[[:digit:]]\z};
+    return if
+        exists $k->{$name} or
+        "$sigil$name" =~ m{\A\$[[:digit:]]+\z};
 
     if (exists $p->{'gv_symbols'}{$name} or
         $name =~ $p->{'gv_match'} ) {
@@ -121,7 +128,7 @@ sub rename_pad {
     return $dict->{$name} if exists $dict->{$name};
 
     $dict->{$name} = $name;
-    return $dict->{$name} = lc $sigil . $self->next_short_dict_symbol;
+    return $dict->{$name} = $sigil . $self->next_short_dict_symbol;
 }
 
 sub rename_gv {
@@ -129,18 +136,32 @@ sub rename_gv {
     my $name = shift;
     my $p = $self->{+__PACKAGE__};
 
-    return $name unless $self->gv_should_be_renamed( $name );
+    my $rv = (caller 5)[3];
+    my $sigil = $rv =~ /pp_rv2cv$/         ? '&'  :
+                $rv =~ /pp_gv$/            ? ''   :
+                $rv =~ /pp_rv2hv$/         ? '%'  :
+                $rv =~ /pp_rv2gv$/         ? '*'  :
+                ($rv =~ /pp_gvsv$/ or
+                 $rv =~ /pp_rv2sv$/)       ? '$' :
+                ($rv =~ /pp_av2arylen$/ or
+                 $rv =~ /pp_aelemfast$/ or
+                 $rv =~ /pp_rv2av$/)       ? '@' :
+                '';
+
+    return $name unless $self->gv_should_be_renamed( $sigil, $name );
 
     my $dict = $p->{'gv_symbols'};
-    return $dict->{$name} if exists $dict->{$name};
-    return $dict->{$name} = ucfirst $self->next_long_dict_symbol;
+    return $dict->{"$sigil$name"} if exists $dict->{"$sigil$name"};
+    return $dict->{"$sigil$name"} = $self->next_long_dict_symbol;
 }
+
+## OVERRIDE METHODS FROM B::Deparse
 
 sub new {
     my $class = shift;
     my $self = $class->SUPER::new( @_ );
     my $p = $self->{+__PACKAGE__} = {};
-    $p->{'unknown_dict_file'} = '/usr/share/dict/stop';
+    $p->{'unknown_dict_file'} = undef;
     $p->{'unknown_dict_data'} = undef;
     $p->{'user_config'} = undef;
     $p->{'gv_match'} = qw/\A[[:lower:][:digit:]_]+\z/;
@@ -166,24 +187,84 @@ sub new {
     return $self;
 }
 
+BEGIN {
+    # B::perlstring was added in 5.8.0
+    if ( *B::perlstring{CODE} ) {
+	*perlstring = *B::perlstring{CODE};
+    } else {
+        *perlstring = sub { '"' . quotemeta( shift() ) . '"' };
+    }
+}
+
 sub compile {
     my(@args) = @_;
     return sub {
         my $source = '';
-        my $self = B::Deobfuscate->new(@args);
-        $self->stash_subs("main");
-        $self->{'curcv'} = B::main_cv;
-        $self->walk_sub(B::main_cv, B::main_start);
-        $source .= join '', $self->print_protos;
-        @{$self->{'subs_todo'}} =
-          sort {$a->[0] <=> $b->[0]} @{$self->{'subs_todo'}};
-        $source .= join '', $self->indent($self->deparse(B::main_root, 0)), "\n"
-          unless B::Deparse::null B::main_root ;
-        my @text;
-        while (scalar(@{$self->{'subs_todo'}})) {
-            push @text, $self->next_todo;
+        my $self = __PACKAGE__->new(@args);
+	# First deparse command-line args
+	if (defined $^I) { # deparse -i
+	    $source .= q(BEGIN { $^I = ).perlstring($^I).qq(; }\n);
+	}
+        if ($^W) { # deparse -w
+            $source .= qq(BEGIN { \$^W = $^W; }\n);
         }
-        $source .= join '', $self->indent(join("", @text)), "\n" if @text;
+        if ($/ ne "\n" or defined $O::savebackslash) { # deparse -l -0
+            my $fs = perlstring($/) || 'undef';
+            my $bs = perlstring($O::savebackslash) || 'undef';
+            $source .= qq(BEGIN { \$/ = $fs; \$\\ = $bs; }\n);
+        }
+	
+	if ($^V ge v5.8.0) {
+	    my @BEGINs = !( *B::begin_av{CODE} and
+			    B::begin_av->isa('B::AV') ) ? () :
+			    B::begin_av->ARRAY;
+	    my @INITs  = B::init_av->isa('B::AV') ? B::init_av->ARRAY : ();
+	    my @ENDs   = !( *B::end_av{CODE} and
+			    B::end_av->isa('B::AV') ) ? () :
+			    B::end_av->ARRAY;
+	    for my $block (@BEGINs, @INITs, @ENDs) {
+		$self->todo($block, 0);
+	    }
+
+	    $self->stash_subs;
+	    $self->{'curcv'} = B::main_cv;
+	    $self->{'curcvlex'} = undef;
+	} else {
+	    # 5.6.x
+	    $self->stash_subs('main');
+	    $self->{'curcv'} = B::main_cv;
+	    $self->walk_sub(B::main_cv, B::main_start);
+	}
+	
+	$source .= join "\n", $self->print_protos;
+	@{$self->{'subs_todo'}} =
+	  sort {$a->[0] <=> $b->[0]} @{$self->{'subs_todo'}};
+	$source .= join "\n",
+	    $self->indent($self->deparse(B::main_root, 0)), "\n"
+	    unless B::Deparse::null B::main_root ;
+	my @text;
+	while (scalar(@{$self->{'subs_todo'}})) {
+	    push @text, $self->next_todo;
+	}
+	$source .= join "\n", $self->indent(join("", @text)), "\n" if @text;
+
+	# Print __DATA__ section, if necessary
+	no strict 'refs';
+	my $laststash = defined $self->{'curcop'}
+	    ? $self->{'curcop'}->stash->NAME : $self->{'curstash'};
+	if (defined *{$laststash."::DATA"}) {
+	    if (eof *{$laststash."::DATA"}) {
+		# I think this only happens when using the module on itself.
+		{
+		    local $/ = "__DATA__\n";
+		    seek *{$laststash."::DATA"}, 0, 0;
+		    readline *{$laststash."::DATA"};
+		}
+	    }
+	    
+	    $source .= "__DATA__\n";
+	    $source .= join '', readline(*{$laststash."::DATA"});
+	}
 
         my $p = $self->{+__PACKAGE__};
         my %dump = ( lexicals => $p->{'pad_symbols'},
@@ -217,191 +298,519 @@ sub gv_name {
 
 1;
 
-__END__
+# The following list of words is compiled from the PGP freeware source code
+# in the file named clients/pgp/shared/pgpHashWords.c.
 
-=head1 NAME
-
-B::Deobfuscate - Extension to B::Deparse for use in de-obfuscating source code
-
-=head1 SYNOPSIS
-
-  perl -MO=Deobfuscate,-csynthetic.yml,-y synthetic.pl
-
-=head1 DESCRIPTION
-
-B::Deobfuscate is a backend module for the Perl compiler that generates perl
-source code, based on the internal compiled structure that perl itself
-creates after parsing a program. It adds symbol renaming functions to the
-B::Deparse module. An obfuscated program is already parsed and interpreted
-correctly by the B::Deparse program. Unfortunately, if the obfuscation
-involved variable renaming then the resulting program also has obfuscated
-symbols.
-
-This module takes the last step and fixes names like $z5223ed336 to be a word
-from a dictionary. While the name still isn't meaningful it is at least easier
-to distinguish and read. Here are two examples - one from B::Deparse and one
-from B::Deobfuscate.
-
-After B::Deparse:
-
-  if (@z6a703c020a) {
-      (my($z5a5fa8125d, $zcc158ad3e0) = File::Temp::tempfile('UNLINK', 1));
-      print($z5a5fa8125d "=over 8\n\n");
-      (print($z5a5fa8125d @z6a703c020a) or die((((q[Can't print ] . $zcc158ad3e0) . ': ') . $!)));
-      print($z5a5fa8125d "=back\n");
-      (close(*$z5a5fa8125d) or die((((q[Can't close ] . *$za5fa8125d) . ': ' . $!)));
-      (@z8374cc586e = $zcc158ad3e0);
-      ($z9e5935eea4 = 1);
-  }
-
-After B::Deobfuscate:
-
-  if (@parenthesises) {
-      (my($scrupulousity, $postprocesser) = File::Temp::tempfile('UNLINK', 1));
-      print($scrupulousity "=over 8\n\n");
-      (print($scrupulousity @parenthesises) or die((((q[Can't print ] . $postprocesser) . ': ') . $!)));
-      print($scrupulousity "=back\n");
-      (close(*$scrupulousity) or die((((q[Can't close ] . *$postprocesser) . ': ') . $!)));
-      (@interruptable = $postprocesser);
-      ($propagandaist = 1);
-  }
-
-You'll note that the only real difference is that instead of variable names
-like $z9e5935eea4 you get $propagandist.
-
-Please note that this module is mainly new and untested code and is
-still under development, so it may change in the future.
-
-=head1 OPTIONS
-
-As with all compiler backend options, these must follow directly after
-the '-MO=Deobfuscate', separated by a comma but not any white space.
-All options defined in B::Deparse are supported here - see the B::Deparse
-documentation page to see what options are provided and how to use them.
-
-=over 4
-
-=item B<-d>I<DICTIONARY>
-
-Normally B::Deobfuscate reads the dictionary file at /usr/share/dict/stop. If
-you would like to specify a different dictionary follow the -d parameter with
-the path the file. The path may not have commas in it and only lines in the
-dictionary that do not match /\W/ will be used. The entire dictionary will be
-loaded into memory at once.
-
-  -d/usr/share/dict/stop
-
-=item B<-m>I<REGEX>
-
-Supply a different regular expression for deciding which symbols to rename.
-The default value is /\A[[:lower:][:digit:]_]+\z/. Your expression must be
-delimited by the '/' characters and you may not use that character within the
-expression. That shouldn't be an issue because '/' isn't valid in a symbol
-name anyway.
-
-  -a/\A[[:lower:][:digit:]_]+\z/
-
-=item B<-y>
-
-print two B<YAML> documents to STDOUT instead of the deparsed source code.
-The first document is a configuration document suitable for use with the B<-c>
-parameter. The second document is the deparsed source code. Use this feature
-to generate a configuration document for further, iterative reverse engineering.
-
-=item B<-c>I<FILENAME>
-
-Supply a filename to a B<YAML> configuration file. Normally you would generate this
-file by saving the results of the B<-y> parameter to a file. You can then edit the
-file to provide your own names for symbols and not rely on the random symbol picker
-in B<B::Deobfuscate>. You may create your own B<YAML> configuration file as well.
-
-=back
-
-=head1 CONFIGURATION FILE
-
-The B::Deobfuscation symbol renamer can be controlled with by a configuration file.
-Use of this feature requires the L<YAML> module be installed.
-
- dictionary: '/usr/share/dict/propernames'
- global_regex: '(?:)'
- globals:
-   kSDsfDS: Slartibartfast
-   HGFdsfds: Triantaphyllos
- lexicals:
-   '$SdfSd': '$No'
-   '$GsdDd': '$Ed'
-   '$Ksdfs': '$Ji'
-
-The following keys are recognized:
-
-=over 4
-
-=item B<dictionary>
-
-This is a filename path to the operative dictionary.
-
- dictionary: /usr/share/dict/stop
-
-=item B<global_regex>
-
-This regular expression tests global symbols. Only symbols that match this
-expression may be renamed. The default value is '\A[[:lower:][:digit:]_]\z/.
-In perl, global symbols are independent of their sigil so the values being
-tested are bare. Future versions of B::Deobfuscate may add the sigil to the
-symbol name.
-
- global_regex: '\A[[:lower:][:digit:]_]\z'
-
-=item B<globals>
-
-This is a hash detailing symbol names as used in the original source and the
-name used in the deobfuscated source. For example - if the original source
-has a variable named @z12345 and you wish to rename all occurrances to 
-@URLList then the hash would associate 'z12345' with 'URLList'. The dictionary
-picker fills these values in automatically.
-
-If you wish to prevent B::Deobfuscate from renaming a symbol then specify the
-new value as '~' (which in YAML terms is undef).
-
- globals:
-   catfile: ~
-   opt_n: ~
-   opt_t: ~
-   opt_u: ~
-   z1234567890: Postprocesser
-   z2345678901: Constructable
-   z3456789012: Photosynthesises
-   z4567890123: Undiscriminate
-   z5678901234: Parenthesises
-   z6789012345: Animadvertion
-
-=item B<lexicals>
-
-Lexicals is a hash exactly like `globals' except that all the symbol names
-include the sigil which doesn't currently happen for globals.
-
- lexicals:
-   '$k1234567890': '$ivs'
-   '$k2345678901': '$ehs'
-   '$k3456789012': '$ans'
-   '$k4567890123': '$ons'
-   '$k5678901234': '$ofs'
-   '$k6789012345': '$gos'
-   '$k7890123456': '$dus'
-   '$k8901234567': '$iis'
-   '$k9012345678': '$ats'
-   '$k0123456780': '$ets'
-
-=back
-
-=head1 AUTHOR
-
-Joshua b. Jore <jjore@cpan.org>
-
-=head1 SEE ALSO
-
-L<B::Deparse>
-L<http://www.perlmonks.org/index.pl?node_id=243011>
-L<http://www.perlmonks.org/index.pl?node_id=244604>
-
-=cut
+__DATA__
+adroitness
+adviser
+aftermath
+aggregate
+alkali
+almighty
+amulet
+amusement
+antenna
+applicant
+Apollo
+armistice
+article
+asteroid
+Atlantic
+atmosphere
+autopsy
+Babylon
+backwater
+barbecue
+belowground
+bifocals
+bodyguard
+bookseller
+borderline
+bottomless
+Bradbury
+bravado
+Brazilian
+breakaway
+Burlington
+businessman
+butterfat
+Camelot
+candidate
+cannonball
+Capricorn
+caravan
+caretaker
+celebrate
+cellulose
+certify
+chambermaid
+Cherokee
+Chicago
+clergyman
+coherence
+combustion
+commando
+company
+component
+concurrent
+confidence
+conformist
+congregate
+consensus
+consulting
+corporate
+corrosion
+councilman
+crossover
+crucifix
+cumbersome
+customer
+Dakota
+decadence
+December
+decimal
+designing
+detector
+detergent
+determine
+dictator
+dinosaur
+direction
+disable
+disbelief
+disruptive
+distortion
+document
+embezzle
+enchanting
+enrollment
+enterprise
+equation
+equipment
+escapade
+Eskimo
+everyday
+examine
+existence
+exodus
+fascinate
+filament
+finicky
+forever
+fortitude
+frequency
+gadgetry
+Galveston
+getaway
+glossary
+gossamer
+graduate
+gravity
+guitarist
+hamburger
+Hamilton
+handiwork
+hazardous
+headwaters
+hemisphere
+hesitate
+hideaway
+holiness
+hurricane
+hydraulic
+impartial
+impetus
+inception
+indigo
+inertia
+infancy
+inferno
+informant
+insincere
+insurgent
+integrate
+intention
+inventive
+Istanbul
+Jamaica
+Jupiter
+leprosy
+letterhead
+liberty
+maritime
+matchmaker
+maverick
+Medusa
+megaton
+microscope
+microwave
+midsummer
+millionaire
+miracle
+misnomer
+molasses
+molecule
+Montana
+monument
+mosquito
+narrative
+nebula
+newsletter
+Norwegian
+October
+Ohio
+onlooker
+opulent
+Orlando
+outfielder
+Pacific
+pandemic
+Pandora
+paperweight
+paragon
+paragraph
+paramount
+passenger
+pedigree
+Pegasus
+penetrate
+perceptive
+performance
+pharmacy
+phonetic
+photograph
+pioneer
+pocketful
+politeness
+positive
+potato
+processor
+provincial
+proximate
+puberty
+publisher
+pyramid
+quantity
+racketeer
+rebellion
+recipe
+recover
+repellent
+replica
+reproduce
+resistor
+responsive
+retraction
+retrieval
+retrospect
+revenue
+revival
+revolver
+sandalwood
+sardonic
+Saturday
+savagery
+scavenger
+sensation
+sociable
+souvenir
+specialist
+speculate
+stethoscope
+stupendous
+supportive
+surrender
+suspicious
+sympathy
+tambourine
+telephone
+therapist
+tobacco
+tolerance
+tomorrow
+torpedo
+tradition
+travesty
+trombonist
+truncated
+typewriter
+ultimate
+undaunted
+underfoot
+unicorn
+unify
+universe
+unravel
+upcoming
+vacancy
+vagabond
+vertigo
+Virginia
+visitor
+vocalist
+voyager
+warranty
+Waterloo
+whimsical
+Wichita
+Wilmington
+Wyoming
+yesteryear
+Yucatan
+aardvark
+absurd
+accrue
+acme
+adrift
+adult
+afflict
+ahead
+aimless
+Algol
+allow
+alone
+ammo
+ancient
+apple
+artist
+assume
+Athens
+atlas
+Aztec
+baboon
+backfield
+backward
+banjo
+beaming
+bedlamp
+beehive
+beeswax
+befriend
+Belfast
+berserk
+billiard
+bison
+blackjack
+blockade
+blowtorch
+bluebird
+bombast
+bookshelf
+brackish
+breadline
+breakup
+brickyard
+briefcase
+Burbank
+button
+buzzard
+cement
+chairlift
+chatter
+checkup
+chisel
+choking
+chopper
+Christmas
+clamshell
+classic
+classroom
+cleanup
+clockwork
+cobra
+commence
+concert
+cowbell
+crackdown
+cranky
+crowfoot
+crucial
+crumpled
+crusade
+cubic
+dashboard
+deadbolt
+deckhand
+dogsled
+dragnet
+drainage
+dreadful
+drifter
+dropper
+drumbeat
+drunken
+Dupont
+dwelling
+eating
+edict
+egghead
+eightball
+endorse
+endow
+enlist
+erase
+escape
+exceed
+eyeglass
+eyetooth
+facial
+fallout
+flagpole
+flatfoot
+flytrap
+fracture
+framework
+freedom
+frighten
+gazelle
+Geiger
+glitter
+glucose
+goggles
+goldfish
+gremlin
+guidance
+hamlet
+highchair
+hockey
+indoors
+indulge
+inverse
+involve
+island
+jawbone
+keyboard
+kickoff
+kiwi
+klaxon
+locale
+lockup
+merit
+minnow
+miser
+Mohawk
+mural
+music
+necklace
+Neptune
+newborn
+nightbird
+Oakland
+obtuse
+offload
+optic
+orca
+payday
+peachy
+pheasant
+physique
+playhouse
+Pluto
+preclude
+prefer
+preshrunk
+printer
+prowler
+pupil
+puppy
+python
+quadrant
+quiver
+quota
+ragtime
+ratchet
+rebirth
+reform
+regain
+reindeer
+rematch
+repay
+retouch
+revenge
+reward
+rhythm
+ribcage
+ringbolt
+robust
+rocker
+ruffled
+sailboat
+sawdust
+scallion
+scenic
+scorecard
+Scotland
+seabird
+select
+sentence
+shadow
+shamrock
+showgirl
+skullcap
+skydive
+slingshot
+slowdown
+snapline
+snapshot
+snowcap
+snowslide
+solo
+southward
+soybean
+spaniel
+spearhead
+spellbind
+spheroid
+spigot
+spindle
+spyglass
+stagehand
+stagnate
+stairway
+standard
+stapler
+steamship
+sterling
+stockman
+stopwatch
+stormy
+sugar
+surmount
+suspense
+sweatband
+swelter
+tactics
+talon
+tapeworm
+tempest
+tiger
+tissue
+tonic
+topmost
+tracker
+transit
+trauma
+treadmill
+Trojan
+trouble
+tumor
+tunnel
+tycoon
+uncut
+unearth
+unwind
+uproot
+upset
+upshot
+vapor
+village
+virus
+Vulcan
+waffle
+wallet
+watchword
+wayside
+willow
+woodlark
+Zulu
